94d54fee4f26fdc3e4bb4411516f1891f6060c2f
Carlos Guisao <titofranco@gmail.com>
2012-07-23 16:08:44 -0500

    refactored side_pannel

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -8,11 +8,11 @@ class MapController < ApplicationController
 
     path = Roadmap.get_path(lat_start, long_start, lat_end, long_end)
 
-    if !path[:msg_error].blank?
+    unless path[:msg_error].blank?
       res = {:success => false, :content => path[:msg_error]}
       render :text => res.to_json
     else
-      route_explain = SidePannel.explainRoute(path[:info_path])
+      route_explain = SidePannel.explain_route(path[:info_path])
 
       closest_init = (path[:info_path].first)[:roadmap_id]
       closest_final = (path[:info_path].last)[:roadmap_id]
@@ -24,7 +24,7 @@ class MapController < ApplicationController
       #new controller
       bus_route = BusesRoute.find_route(closest_init, closest_final)
       unless bus_route.empty?
-        bus_explain = SidePannel.explainBusRoute(bus_route)
+        bus_explain = SidePannel.explain_bus_route(bus_route)
       end
 
       res = {:success => true,  :content => path[:info_path], :bus => bus_route, :route_explain => route_explain, :bus_explain => bus_explain}
--- lib/side_pannel.rb
+++ lib/side_pannel.rb
@@ -2,33 +2,30 @@
 module SidePannel
 
   #With this I'll know where to turn given 2 cardinal points
-  def SidePannel.eval_direction(comes_from, goes_to)
-    tell=nil
+  def self.eval_direction(comes_from, goes_to)
     case comes_from
     when "Norte"
-      tell=where_to_turn_n(goes_to)
+      tell = where_to_turn_n(goes_to)
     when "Sur"
-      tell=where_to_turn_s(goes_to)
+      tell = where_to_turn_s(goes_to)
     when "Oriente"
-      tell=where_to_turn_e(goes_to)
+      tell = where_to_turn_e(goes_to)
     when "Occidente"
-      tell=where_to_turn_w(goes_to)
+      tell = where_to_turn_w(goes_to)
     when "Nororiente"
-      tell=where_to_turn_ne(goes_to)
+      tell = where_to_turn_ne(goes_to)
     when "Noroccidente"
-      tell=where_to_turn_nw(goes_to)
+      tell = where_to_turn_nw(goes_to)
     when "Suroriente"
-      tell=where_to_turn_se(goes_to)
+      tell = where_to_turn_se(goes_to)
     when "Suroccidente"
-      tell=where_to_turn_sw(goes_to)
+      tell = where_to_turn_sw(goes_to)
     end
-
-    return tell
+    tell
   end
 
   #NORTE - NORTH
-  def SidePannel.where_to_turn_n(goes_to)
-
+  def self.where_to_turn_n(goes_to)
     case goes_to
     when "Oriente"
       turn = "a la derecha"
@@ -41,12 +38,11 @@ module SidePannel
     when "Sur"
       turn = "en giro en U"
     end
-    return turn
+    turn
   end
 
   #OCCIDENTE - WEST
-  def SidePannel.where_to_turn_w(goes_to)
-
+  def self.where_to_turn_w(goes_to)
     case goes_to
     when "Norte"
       turn = "a la derecha"
@@ -59,12 +55,11 @@ module SidePannel
     when "Oriente"
       turn = "en giro en U"
     end
-    return turn
+    turn
   end
 
   #ORIENTE - EAST
-  def SidePannel.where_to_turn_e(goes_to)
-
+  def self.where_to_turn_e(goes_to)
     case goes_to
     when "Norte"
       turn = "a la izquierda"
@@ -77,14 +72,12 @@ module SidePannel
     when "Occidente"
       turn = "en giro en U"
     end
-    return turn
+    turn
   end
 
   #SUR - SOUTH
-  def SidePannel.where_to_turn_s(goes_to)
-
+  def self.where_to_turn_s(goes_to)
     case goes_to
-
     when "Oriente"
       turn = "a la izquierda"
     when "Noroccidente", "Suroccidente"
@@ -96,33 +89,28 @@ module SidePannel
     when "Norte"
       turn = "en giro en U"
     end
-    return turn
+    turn
   end
 
   #SUROCCIDENTE - SOUTHWEST
-  def SidePannel.where_to_turn_sw(goes_to)
-
+  def self.where_to_turn_sw(goes_to)
     case goes_to
     when "Norte"
       turn = "a la derecha"
     when "Noroccidente", "Occidente"
       turn = "ligeramente a la derecha en dirección " + goes_to
-
     when "Sur", "Suroriente"
       turn = "ligeramente a la izquierda en dirección " + goes_to
-
     when "Oriente"
       turn = "a la izquierda"
-
     when "Nororiente"
       turn = "en giro en U"
     end
-    return turn
+    turn
   end
 
   #SURORIENTE - SOUTHEAST
-  def SidePannel.where_to_turn_se(goes_to)
-
+  def self.where_to_turn_se(goes_to)
     case goes_to
     when"Norte"
       turn = "a la izquierda"
@@ -135,12 +123,11 @@ module SidePannel
     when "Noroccidente"
       turn = "en giro en U"
     end
-    return turn
+    turn
   end
 
   #NOROCCIDENTE - NORTHWEST
-  def SidePannel.where_to_turn_nw(goes_to)
-
+  def self.where_to_turn_nw(goes_to)
     case goes_to
     when"Oriente"
       turn = "a la derecha"
@@ -153,12 +140,11 @@ module SidePannel
     when "Suroriente"
       turn = "en giro en U"
     end
-    return turn
+    turn
   end
 
   #NORORIENTE - NORTHEAST
-  def SidePannel.where_to_turn_ne(goes_to)
-
+  def self.where_to_turn_ne(goes_to)
     case goes_to
     when"Sur"
       turn = "a la derecha"
@@ -171,167 +157,180 @@ module SidePannel
     when "Suroccidente"
       turn = "en giro en U"
     end
-    return turn
+    turn
   end
 
+  def self.explain_route(route)
 
-  def SidePannel.explainRoute(infoRoute)
-
-    continueStraight, estacion_metro = false
+    continueStraight, metro_station = false
     first_node = true
-    i=0
-    j=1
-    size = infoRoute.length
+    i = 0
+    j = 1
+    size = route.length
     explain, turn, curr_dir, prev_dir, curr_stretch_type, prev_stretch_type, curr_bearing, prev_bearing = nil
 
-    total_distance = getTotalDistanceRoute(infoRoute)
-    total_time = getTimeAprox(total_distance)
+    total_distance = total_distance(route)
 
-    explain = '<li class="route-explain">' +
-      '<b class="header">Indicaciones de ruta a pie para llegar a tu lugar de destino</b>' +
-      '<table><br><tr><td><b>Distancia aproximada: </b></td><td>' + total_distance.to_s +
-      ' metros</td></tr>' +'<tr><td><b>Tiempo aproximado caminando a 3km/h: </b></td><td>' +
-      total_time.to_s + ' minutos</td></tr>' +
-      '</table></li>'
+    explain =
+      '<div class="route-explain">
+         <span class="header">Indicaciones de ruta a pie para llegar a tu lugar de destino</span>
+         <table>
+           <tr>
+             <td class="b">Distancia aproximada:</td>
+             <td>' + total_distance + ' metros</td>
+           </tr>
+           <tr>
+             <td class="b">Tiempo aproximado caminando a 3km/h:</td>
+             <td>'+ avg_time(total_distance) + ' minutos</td>
+           </tr>
+         </table>
+       </div>'
 
-    for i in 0 ... infoRoute.length - 1
-      #I assign (direction and stretch_type) current and previous
-      if i>0
-        prev_stretch_type = infoRoute[i-1][:stretch_type]
-        curr_stretch_type = infoRoute[i][:stretch_type]
-        prev_dir = infoRoute[i-1][:new_direction]
-        curr_dir = infoRoute[i][:new_direction]
+    for i in 0 ... route.length - 1
+      #It assigns (direction and stretch_type) current and previous
+      if i > 0
+        prev_stretch_type = route[i-1][:stretch_type].to_s
+        curr_stretch_type = route[i][:stretch_type].to_s
+        prev_dir = route[i-1][:new_direction].to_s
+        curr_dir = route[i][:new_direction].to_s
       end
 
       if first_node
-
-        explain += '<li id=sidebar-item-' + i.to_s + ' >' +
-          '<a href="#" onclick="javascript:focusPoint(' + i.to_s + ')">' +
-          j.to_s + ". " + "Dirigete en dirección <b>" + infoRoute[i][:direction] +
-          "</b> hacia la " +"<b>"+ infoRoute[i][:way_type_b] +  " " +
-          infoRoute[i][:street_name_b] + " (metros: " + getDistance(i,infoRoute) + ")" +
-          "</b></a></li>"
-
+        explain +=
+          '<li id=sidebar-item-' + i.to_s + ' >
+             <a href="#" onclick="javascript:focusPoint(' + i.to_s + ')">' +
+             j.to_s + ". Dirigete en dirección
+             <span class='b'>" + route[i][:direction] + "</span> hacia la
+             <span>" + route[i][:way_type_b] +  " " + route[i][:street_name_b] +
+             " (metros: " + distance(i, route) + ")</span>
+             </a>
+          </li>"
         first_node = false
         j = j+1
-      elsif prev_dir != curr_dir && curr_stretch_type==1
+      elsif prev_dir != curr_dir and curr_stretch_type.eql?("1")
         turn = eval_direction(prev_dir, curr_dir)
 
-        explain += '<li id=sidebar-item-' + i.to_s+' >' +
-          '<a href="#" onclick="javascript:focusPoint(' + i.to_s + ')">' +
-          j.to_s + ". Voltear " + "<b>"+ turn + "</b> por <b>" +
-          infoRoute[i][:way_type_b] +  " " + infoRoute[i][:street_name_b]
+        explain +=
+          '<li id=sidebar-item-' + i.to_s+' >
+             <a href="#" onclick="javascript:focusPoint(' + i.to_s + ')">' +
+             j.to_s + ". Voltear <span class='b'> " + turn + "</span> por
+               <span class='b'>" + route[i][:way_type_b] + " " + route[i][:street_name_b] + "</span>"
 
-        if infoRoute[i][:has_relation]
-          intersection = infoRoute[i][:street_name_b].index('-')
-
-          if intersection
-            explain += '</b> y continúa por <b>' + infoRoute[i][:way_type_b] +
-              ' ' + infoRoute[i][:street_name_b].slice(0, intersection)
-
-          end
+        if route[i][:has_relation]
+          intersection = route[i][:street_name_b].index('-')
+          explain +=
+             'y continúa por <span class="b">' + route[i][:way_type_b] + ' ' + route[i][:street_name_b].slice(0, intersection) if intersection
         end
-        explain +=  " (metros: " + getDistance(i, infoRoute) + ")</b></a></li>"
+        explain +=
+           "  <span class='b'> (metros: " + distance(i, route) + ")</span>
+            </a>
+          </li>"
         j = j+1
         #Here for the metro station
         #If the algorithm choose stretch_type 4 and then 3 it means the next nodes are type 2
         #therefore a metro's station has been choosen
-      elsif prev_stretch_type==4 && curr_stretch_type==3
-        estacion_metro = true
+      elsif prev_stretch_type.eql?("4") and curr_stretch_type.eql?("3")
+        metro_station = true
 
-      elsif prev_stretch_type==3 && curr_stretch_type==2 && estacion_metro==true
+      elsif prev_stretch_type.eql?("3") and curr_stretch_type.eql?("2") and metro_station
 
-        explain += '<li id=sidebar-item-' + i.to_s + ' >' +
-          '<a href="#" onclick="javascript:focusMetro(' + infoRoute[i-1][:related_id] + ')">' +
-          j.to_s + ". Ve de la estación <b> " + infoRoute[i-1][:common_name_a]
-
+        explain +=
+          '<li id=sidebar-item-' + i.to_s + ' >
+             <a href="#" onclick="javascript:focusMetro(' + route[i-1][:related_id] + ')">' +
+             j.to_s + ". Ve de la estación <span class='b'> " + route[i-1][:common_name_a]
         j = j+1
 
         #if it finds a stretch_type 3 it means it reached the end of the metro station
-      elsif estacion_metro && (prev_stretch_type==2 && curr_stretch_type==3)
-        explain += ' hasta la estación <b>' + infoRoute[i].common_name_a + '</b></a></li>'
+      elsif metro_station and (prev_stretch_type.eql?("2") and curr_stretch_type.eql?("3"))
+        explain +=
+          ' hasta la estación' + route[i].common_name_a + '</span>
+            </a>
+          </li>'
 
         #if it finds a stretch_type 4 is because he got off the train and is going to some street
-      elsif prev_stretch_type==3 && curr_stretch_type==4
+      elsif prev_stretch_type.eql?("3") and curr_stretch_type.eql?("4")
 
-        explain += '<li id=sidebar-item-' + i.to_s+' >' +
-          '<a href="#" onclick="javascript:focusPoint(' + i.to_s + ')">'+ j.to_s +
-          ". Baja de la estación " + infoRoute[i-1][:common_name_a] +
-          " dirigete por el <b>"+ infoRoute[i][:common_name_b] +  " " +
-          infoRoute[i][:street_name_a] + " (metros:" + infoRoute[i][:distance] + ")</b></a></li>"
-
-        estacion_metro = false
+        explain +=
+          '<li id=sidebar-item-' + i.to_s + ' >
+            <a href="#" onclick="javascript:focusPoint(' + i.to_s + ')">' +
+            j.to_s + ". Baja de la estación " + route[i-1][:common_name_a] +
+            " dirigete por el <span class='b'> " + route[i][:common_name_b] +  " " +
+          route[i][:street_name_a] + " (metros:" + route[i][:distance] + ")</span>
+            </a>
+          </li>"
+        metro_station = false
         j = j+1
       end
       #end for
     end
 
-    #If we have more than  2 nodes then we proceed to end the explanation
+    #If we have more than 2 nodes then we proceed to end of the explanation
     if size > 1
       final = nil
-      if infoRoute[size-2][:direction] == infoRoute[size-1][:direction]
+      if route[size-2][:direction] == route[size-1][:direction]
 
         final = j.to_s + ". " +'Continúa hasta encontrar tu lugar de destino' +
-          "<b> (metros: " + getDistance(i+1, infoRoute) + ")</b>"
+          "<span class='b'> (metros: " + distance(i+1, route) + ")</span>"
 
       else
-        turn = eval_direction(infoRoute[size-2][:direction],infoRoute[size-1][:direction])
+        turn = eval_direction(route[size-2][:direction],route[size-1][:direction])
 
-        final = j.to_s + ". " + 'Voltea <b> '+ turn +
-          '</b> hasta llegar a tu lugar destino </b>' +
-          "<b> (metros: " + getDistance(i+1, infoRoute) + ")</b>"
+        final = j.to_s + ". " + 'Voltea <span class="b"> ' + turn + '</span>
+           hasta llegar a tu lugar destino' +
+          "<span class='b'> (metros: " + distance(i+1, route) + ")</span>"
 
       end
 
-      explain += '<li id=sidebar-item-'+(i+1).to_s+' >' +
-        '<a href="#" onclick="javascript:focusPoint('+(i+1).to_s+')">' +
-        final + '</a></li>'
+      explain +=
+        '<li id=sidebar-item-' + (i+1).to_s + ' >' +
+          '<a href="#" onclick="javascript:focusPoint('+(i+1).to_s+')">' + final + '
+          </a>
+        </li>'
     else
-      explain += '<li id=sidebar-item-'+ 0 +' >' +
-        '<a href="#" onclick="javascript:focusPoint('+ 0 +')">'+
-        "1. Dirigete en dirección <b>" + infoRoute[i][:direction] +
-        "</b> hacia la <b>" + infoRoute[i][:way_type_b] +  " " + infoRoute[i][:street_name_b] +
-        "</b> hasta llegar a tu lugar de destino (metros: "+ getDistance(i+1,infoRoute) +
-        ")</b></a></li>"
+      explain +=
+        '<li id=sidebar-item-' + 0 + ' >
+          <a href="#" onclick="javascript:focusPoint('+ 0 +')">
+          1. Dirigete en dirección <span class="b">' + route[i][:direction] + "</span>
+          hacia la <span class='b'>" + route[i][:way_type_b] +  " " + route[i][:street_name_b] + "</span>
+          hasta llegar a tu lugar de destino (metros: " + distance(i + 1, route) + ")</span>
+          </a>
+        </li>"
     end
     explain
   end
 
   #It gets the distance of a group of trajectories that have the same related_id
-  def SidePannel.getDistance(index, infoRoute)
-    total_distance = 0
-    for i in 0 ... infoRoute.length
-      id_related = infoRoute[index][:related_id]
-      if infoRoute[i][:related_id] == id_related
-        total_distance += infoRoute[i][:distance].to_f
-      end
-    end
-    return sprintf('%.2f', total_distance).to_s
+  def self.distance(index, route)
+    id_related = route[index][:related_id]
+    distance = route.sum{ |r| id_related == r[:related_id] ? r[:distance].to_f : 0 }
+    sprintf('%.2f', distance)
   end
 
-  def SidePannel.getTotalDistanceRoute(infoRoute)
-    total_distance = 0
-    infoRoute.each{ |ir| total_distance += ir[:distance].to_f }
-    return sprintf('%2.f',total_distance).to_s
+  def self.total_distance(route)
+    sprintf('%2.f', route.sum{ |r| r[:distance].to_f})
   end
 
-  def SidePannel.getTimeAprox(total_distance)
-    time_aprox= (total_distance.to_f * 60)/3000
-    return sprintf('%2.f', time_aprox)
+  def self.avg_time(total_distance)
+    sprintf('%2.f', (total_distance.to_f * 60)/3000)
   end
 
-
-  def SidePannel.explainBusRoute(infoBuses)
-
-    explain = '<hr><li class="route-explain">'+
-      '<b class="header">Indicación de ruta de bus cercana</b></li>'
-    for i in 0 ... infoBuses.length - 1
-      if(infoBuses[i][:bus_id] != infoBuses[i+1][:bus_id])
-        explain += '<span class=buses-checkbox> <li id=sidebar-item-bus'+ infoBuses[i][:bus_id].to_s+' >'+
-          '<input type="checkbox" name="chk'+infoBuses[i][:bus_id].to_s+'"' +
-          ' onClick="javascript:drawSelectedPolyline_bus('+"this,"+infoBuses[i][:bus_id].to_s+')">' +
-          " Ruta numero " + infoBuses[i][:bus_id].to_s + "</li></span>"
-      end
+  def self.explain_bus_route(bus_route)
+    buses = bus_route.map{|bus| bus.values_at(:bus_id)}.uniq.flatten
+    buses.pop # Last element is a fake record
+    explain =
+      "<hr>
+       <div class='route-explain'>
+         <span class='header'>Indicación de ruta de bus cercana</span>
+       </div>"
+    buses.each do |bus|
+      explain +=
+        '<span class=buses-checkbox>
+           <li id=sidebar-item-bus' + bus.to_s + '>
+             <input type="checkbox" name="chk' + bus.to_s +
+             '"onClick="javascript:drawSelectedPolyline_bus('"this," + bus.to_s + ')">' +
+             " Ruta numero " + bus.to_s +
+           "</li>
+        </span>"
     end
     explain
   end
